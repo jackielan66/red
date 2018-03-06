6889e5667751f9148c60beb4f1226b18192bbea9
name <13700@gmail.com>
2018-03-06 15:17:10 +0800

    first

---
+++ web/src/service/domains.js
@@ -0,0 +1,18 @@
+// 感谢群友支持的服务器
+export default [
+  // 官方
+  'https://hongbao.xxooweb.com',
+  // Promise
+  'http://hongbao.dingjian.name',
+  // 哈哈哈哈。
+  'http://101.226.171.170:3007',
+  // 简体
+  'http://hongbao.lte.pw:3007',
+  // 等
+  'http://118.126.88.246:3007',
+  // 贤弟
+  'https://hbapi.moexian.com',
+  // W
+  'http://45.77.165.37:3007',
+  'http://localhost:3007'
+]
